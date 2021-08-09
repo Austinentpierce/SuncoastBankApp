54f8452141c885304966940ae9661cf3daeff0d4
Austin Pierce <Austinentpierce@gmail.com>
2021-08-08 23:27:41 -0400

    working on continuing to add a new transaction. withdraw amount and deposit amount  from selected accounts

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SuncoastBankApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public int Amount { get; set; }
+        public string Type { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Account { get; set; }
+
     }
-  }
+
+    class Program
+    {
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+
+            var userInput = Console.ReadLine();
+            var userInputAsNumber = int.Parse(userInput);
+
+            return userInputAsNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            var transactions = new List<Transaction>()
+      {
+        new Transaction {Type = "Deposit", Amount = 100, Account = "Checking" },
+        new Transaction {Type = "Deposit", Amount = 4000, Account = "Saving" },
+        new Transaction {Type = "Withdraw", Amount = 500, Account = "Checking" },
+      };
+
+
+        }
+    }
+
+
+
 }
