616cfefed8ee8cb596025d973e9d01dcbd3bbb1e
Austin Pierce <Austinentpierce@gmail.com>
2021-08-08 23:50:35 -0400

    added menu options, now working on if statements for each menu choice

--- Program.cs
+++ Program.cs
@@ -36,12 +36,30 @@ namespace SuncoastBankApp
         static void Main(string[] args)
         {
             var transactions = new List<Transaction>()
-      {
-        new Transaction {Type = "Deposit", Amount = 100, Account = "Checking" },
-        new Transaction {Type = "Deposit", Amount = 4000, Account = "Saving" },
-        new Transaction {Type = "Withdraw", Amount = 500, Account = "Checking" },
-      };
+          {
+          new Transaction {Type = "Deposit", Amount = 500, Account = "Checking" },
+          new Transaction {Type = "Deposit", Amount = 4000, Account = "Savings" },
+          new Transaction {Type = "Withdrawl", Amount = 375, Account = "Checking" },
+          new Transaction {Type = "Withdrawl", Amount = 2350, Account = "Savings"}
+          };
+            var userHasChosenToQuit = false;
+            while (userHasChosenToQuit == false)
+            {
+                Console.WriteLine("Menu Choices");
+                Console.WriteLine();
+                Console.WriteLine("Deposit");
+                Console.WriteLine("Transfer");
+                Console.WriteLine("Withdraw");
+                Console.WriteLine("Balance");
+                Console.WriteLine("History");
+                Console.WriteLine("Quit");
 
+                var choice = PromptForString("Choice : ").ToUpper();
+                if (choice == "Deposit")
+                {
+                    Console.WriteLine($)
+                }
+            }
 
         }
     }
